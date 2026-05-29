80769016df601d08bea5075ec1471f6a5fa65ccb
borshstudio <borshstudio@mail.ru>
2026-05-29 19:19:33 +0300

    Обновлен конфиг

--- app/config.py
+++ app/config.py
@@ -1,2 +1,6 @@
-MAX_URLS = 5
-BASE_URL = "http://127.0.0.1:8000"
+import os
+
+class Config:
+    MAX_URLS = int(os.getenv("MAX_URLS", 5))
+    BASE_URL = os.getenv("BASE_URL", "http://127.0.0.1:8000")
+    DB_PATH = os.getenv("DB_PATH", "urls.db")
