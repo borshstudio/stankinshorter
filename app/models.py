241c5c7328bf653d7dcab84c6b4e58d2e46c8fd6
borshstudio <borshstudio@mail.ru>
2026-05-29 19:21:10 +0300

    Добавлен класс сокращенной ссылки

--- app/models.py
+++ app/models.py
@@ -6,11 +6,16 @@ class UrlItem:
     Класс одной сокращённой ссылки.
     """
 
-    def __init__(self, original_url: str, short_code: str):
+    def __init__(
+        self,
+        id: int,
+        original_url: str,
+        short_code: str,
+        clicks: int,
+        created_at: str
+    ):
+        self.id = id
         self.original_url = original_url
         self.short_code = short_code
-        self.clicks = 0
-        self.created_at = datetime.now()
-
-    def add_click(self):
-        self.clicks += 1
+        self.clicks = clicks
+        self.created_at = created_at
